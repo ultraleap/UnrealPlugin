c99d3b4c50b8ac1eea0d893244bd9d0ff12e0873
getnamo <getnamo@gmail.com>
2018-06-13 22:33:48 +0100

    scaffold p2

--- Source/LeapMotion/Private/LeapLiveLink.cpp
+++ Source/LeapMotion/Private/LeapLiveLink.cpp
@@ -1,14 +1,32 @@
 #include "LeapLiveLink.h"
+#include "CoreMinimal.h"
 #include "LiveLinkProvider.h"
+
+void OnConnectionStatusChanged()
+{
+	//refresh?
+}
 
 void FLeapLiveLinkProducer::Startup()
 {
 	LiveLinkProvider = ILiveLinkProvider::CreateLiveLinkProvider(TEXT("Leap Motion Live Link"));
-	//ConnectionStatusChangedHandle = LiveLinkProvider->RegisterConnStatusChangedHandle(FLiveLinkProviderConnectionStatusChanged::FDelegate::CreateStatic(&OnConnectionStatusChanged));
+	ConnectionStatusChangedHandle = LiveLinkProvider->RegisterConnStatusChangedHandle(FLiveLinkProviderConnectionStatusChanged::FDelegate::CreateStatic(&OnConnectionStatusChanged));
+
+	SubjectName = TEXT("Leap Motion");
 }
 
 void FLeapLiveLinkProducer::ShutDown()
 {
-
+	LiveLinkProvider->UnregisterConnStatusChangedHandle(ConnectionStatusChangedHandle);
 }
 
+void FLeapLiveLinkProducer::LinkToSkeleton(const UBodyStateSkeleton* Skeleton)
+{
+	//Initialize the subject
+	LiveLinkProvider->UpdateSubject(SubjectName, SubjectBoneNames, SubjectBoneParents);
+}
+
+void FLeapLiveLinkProducer::UpdateFromBodyState(const UBodyStateSkeleton* Skeleton)
+{
+	LiveLinkProvider->UpdateSubjectFrame(SubjectName, SubjectBoneTransforms, SubjectCurves, FApp::GetCurrentTime());
+}
--- Source/LeapMotion/Private/LeapLiveLink.h
+++ Source/LeapMotion/Private/LeapLiveLink.h
@@ -3,11 +3,23 @@
 #include "Runtime/LiveLinkInterface/Public/LiveLinkTypes.h"
 #include "Runtime/LiveLinkInterface/Public/ILiveLinkClient.h"
 #include "Runtime/LiveLinkMessageBusFramework/Public/LiveLinkProvider.h"
+#include "BodyStateSkeleton.h"
 
 class FLeapLiveLinkProducer
 {
-	TSharedPtr<ILiveLinkProvider> LiveLinkProvider;
-
+public:
 	void Startup();
 	void ShutDown();
+
+	void LinkToSkeleton(const UBodyStateSkeleton* Skeleton);
+	void UpdateFromBodyState(const UBodyStateSkeleton* Skeleton);
+
+protected:
+	FDelegateHandle ConnectionStatusChangedHandle;
+	TSharedPtr<ILiveLinkProvider> LiveLinkProvider;
+	FName SubjectName;
+	TArray<FName> SubjectBoneNames;
+	TArray<int32> SubjectBoneParents;
+	TArray<FTransform> SubjectBoneTransforms;
+	TArray<FLiveLinkCurveElement> SubjectCurves;
 };
