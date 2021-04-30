6cf39b634283238a87fbd98e818dfd7c9758554a
AzureAD\JimSimons <jim.simons@ultraleap.com>
2021-04-30 17:29:45 +0100

    Test with Mesh from UE mannequin

--- Source/BodyState/Private/AnimNode_ModifyBodyStateMappedBones.cpp
+++ Source/BodyState/Private/AnimNode_ModifyBodyStateMappedBones.cpp
@@ -1,55 +1,65 @@
 // Copyright 1998-2020 Epic Games, Inc. All Rights Reserved.
 
 #include "AnimNode_ModifyBodyStateMappedBones.h"
-#include "AnimationRuntime.h"
-#include "Runtime/Engine/Public/Animation/AnimInstanceProxy.h"
-#include "BoneControllers/AnimNode_SkeletalControlBase.h"
 
-FAnimNode_ModifyBodyStateMappedBones::FAnimNode_ModifyBodyStateMappedBones()
-	: FAnimNode_SkeletalControlBase()
+#include "AnimationRuntime.h"
+#include "BoneControllers/AnimNode_SkeletalControlBase.h"
+#include "Runtime/Engine/Public/Animation/AnimInstanceProxy.h"
+
+FAnimNode_ModifyBodyStateMappedBones::FAnimNode_ModifyBodyStateMappedBones() : FAnimNode_SkeletalControlBase()
 {
 	WorldIsGame = false;
 	Alpha = 1.f;
 }
 
-void FAnimNode_ModifyBodyStateMappedBones::OnInitializeAnimInstance(const FAnimInstanceProxy* InProxy, const UAnimInstance* InAnimInstance) {
+void FAnimNode_ModifyBodyStateMappedBones::OnInitializeAnimInstance(
+	const FAnimInstanceProxy* InProxy, const UAnimInstance* InAnimInstance)
+{
 	Super::OnInitializeAnimInstance(InProxy, InAnimInstance);
 	BSAnimInstance = Cast<UBodyStateAnimInstance>(InAnimInstance);
 }
 
-void FAnimNode_ModifyBodyStateMappedBones::EvaluateSkeletalControl_AnyThread(FComponentSpacePoseContext& Output, TArray<FBoneTransform>& OutBoneTransforms) {
+void FAnimNode_ModifyBodyStateMappedBones::EvaluateSkeletalControl_AnyThread(
+	FComponentSpacePoseContext& Output, TArray<FBoneTransform>& OutBoneTransforms)
+{
 	EvaluateComponentPose_AnyThread(Output);
 }
 
-
-void FAnimNode_ModifyBodyStateMappedBones::EvaluateComponentPose_AnyThread(FComponentSpacePoseContext& Output) {
+void FAnimNode_ModifyBodyStateMappedBones::EvaluateComponentPose_AnyThread(FComponentSpacePoseContext& Output)
+{
 	Super::EvaluateComponentPose_AnyThread(Output);
 
+	if (BSAnimInstance->MappedBoneList.Num() == 0)
+	{
+		return;
+	}
+	// return;
 	if (MappedBoneAnimData.BoneMap.Num() == 0 || MappedBoneAnimData.BodyStateSkeleton == nullptr)
 	{
 		UE_LOG(LogTemp, Log, TEXT("Updating cached MappedBoneAnimData"));
 		MappedBoneAnimData = BSAnimInstance->MappedBoneList[0];
 	}
 
-	//If we don't have data driving us, ignore this evaluation
+	// If we don't have data driving us, ignore this evaluation
 	const FBoneContainer& BoneContainer = Output.Pose.GetPose().GetBoneContainer();
 	const float BlendWeight = FMath::Clamp<float>(ActualAlpha, 0.f, 1.f);
 
 	TArray<FBoneTransform> TempTransform;
 
-	//JIM: moved scoped lock around all
+	// JIM: moved scoped lock around all
 	{
 		if (!MappedBoneAnimData.BodyStateSkeleton)
 		{
 			return;
 		}
 		FScopeLock ScopeLock(&MappedBoneAnimData.BodyStateSkeleton->BoneDataLock);
-		//SN: there should be an array re-ordered by hierarchy (parents -> children order)
+		// SN: there should be an array re-ordered by hierarchy (parents -> children order)
 		for (auto CachedBone : MappedBoneAnimData.CachedBoneList)
 		{
 			if (CachedBone.MeshBone.BoneIndex == -1)
 			{
-				UE_LOG(LogTemp, Warning, TEXT("%s has an invalid bone index: %d"), *CachedBone.MeshBone.BoneName.ToString(), CachedBone.MeshBone.BoneIndex);
+				UE_LOG(LogTemp, Warning, TEXT("%s has an invalid bone index: %d"), *CachedBone.MeshBone.BoneName.ToString(),
+					CachedBone.MeshBone.BoneIndex);
 				continue;
 			}
 
@@ -61,23 +71,23 @@ void FAnimNode_ModifyBodyStateMappedBones::EvaluateComponentPose_AnyThread(FComp
 			{
 				NewBoneTM = Output.Pose.GetComponentSpaceTransform(CompactPoseBoneToModify);
 				ComponentTransform = Output.AnimInstanceProxy->GetComponentTransform();
-
 			}
 
-			//Scale
-			//Ignored
+			// Scale
+			// Ignored
 
-			//Rotate
+			// Rotate
 
-			//Bone Space
-			FAnimationRuntime::ConvertCSTransformToBoneSpace(ComponentTransform, Output.Pose, NewBoneTM, CompactPoseBoneToModify, EBoneControlSpace::BCS_ComponentSpace);
+			// Bone Space
+			FAnimationRuntime::ConvertCSTransformToBoneSpace(
+				ComponentTransform, Output.Pose, NewBoneTM, CompactPoseBoneToModify, EBoneControlSpace::BCS_ComponentSpace);
 			FQuat BoneQuat = CachedBone.BSBone->BoneData.Transform.GetRotation();
 
-
-			//Apply pre and post adjustment (Post * (Input * Pre) )
+			// Apply pre and post adjustment (Post * (Input * Pre) )
 			if (!MappedBoneAnimData.PreBaseRotation.ContainsNaN())
 			{
-				BoneQuat = MappedBoneAnimData.OffsetTransform.GetRotation() * (BoneQuat * MappedBoneAnimData.PreBaseRotation.Quaternion());
+				BoneQuat =
+					MappedBoneAnimData.OffsetTransform.GetRotation() * (BoneQuat * MappedBoneAnimData.PreBaseRotation.Quaternion());
 			}
 
 			NewBoneTM.SetRotation(BoneQuat);
@@ -89,20 +99,21 @@ void FAnimNode_ModifyBodyStateMappedBones::EvaluateComponentPose_AnyThread(FComp
 				NewBoneTM.SetTranslation(RotatedTranslation + MappedBoneAnimData.OffsetTransform.GetLocation());
 			}
 
-			//Back to component space
-			FAnimationRuntime::ConvertBoneSpaceTransformToCS(ComponentTransform, Output.Pose, NewBoneTM, CompactPoseBoneToModify, EBoneControlSpace::BCS_ComponentSpace);
+			// Back to component space
+			FAnimationRuntime::ConvertBoneSpaceTransformToCS(
+				ComponentTransform, Output.Pose, NewBoneTM, CompactPoseBoneToModify, EBoneControlSpace::BCS_ComponentSpace);
 
-			//Translate
+			// Translate
 
-			//Workaround ensure our rotations are offset for each bone
+			// Workaround ensure our rotations are offset for each bone
 			TempTransform.Add(FBoneTransform(CachedBone.MeshBone.GetCompactPoseIndex(BoneContainer), NewBoneTM));
 			Output.Pose.LocalBlendCSBoneTransforms(TempTransform, BlendWeight);
 			TempTransform.Reset();
 
-			//Apply transforms to list
-			//OutBoneTransforms.Add(FBoneTransform(CachedBone.MeshBone.GetCompactPoseIndex(BoneContainer), NewBoneTM));
+			// Apply transforms to list
+			// OutBoneTransforms.Add(FBoneTransform(CachedBone.MeshBone.GetCompactPoseIndex(BoneContainer), NewBoneTM));
 		}
-	// JIM: end of new scoped lock position
+		// JIM: end of new scoped lock position
 	}
 }
 
