e0f9a2b1055769b10ef4800e65f39f5358d37d55
AzureAD\JimSimons <jim.simons@ultraleap.com>
2021-07-01 11:48:05 +0100

    FIxed packaging error for editor only var

--- Source/LeapMotion/Private/TickInEditorStaticMeshComponent.cpp
+++ Source/LeapMotion/Private/TickInEditorStaticMeshComponent.cpp
@@ -9,5 +9,7 @@ UTickInEditorStaticMeshComponent::UTickInEditorStaticMeshComponent(const FObject
 	bTickInEditor = true;
 	bAutoActivate = true;
 	bHiddenInGame = true;
+#if WITH_EDITOR
 	bVisualizeComponent = false;
+#endif //WITH_EDITOR
 }
