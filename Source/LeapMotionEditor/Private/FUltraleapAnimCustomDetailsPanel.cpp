a16fb134a212836ebf1c38f3b2f0e214fa99a39b
AzureAD\JimSimons <jim.simons@ultraleap.com>
2021-07-26 14:59:19 +0100

    Irrelevant comment

--- Source/LeapMotionEditor/Private/FUltraleapAnimCustomDetailsPanel.cpp
+++ Source/LeapMotionEditor/Private/FUltraleapAnimCustomDetailsPanel.cpp
@@ -46,10 +46,7 @@ void FUltraleapAnimCustomDetailsPanel::CustomizeDetails(IDetailLayoutBuilder& De
 				.OnClicked(this, &FUltraleapAnimCustomDetailsPanel::ClickedOnButton)	// Binding the OnClick function we want to
 																						// execute when
 																						// this object is clicked
-				.Content()[	   // We create a new slate object inside our button. In this case a text block with the content of
-							   // "Change Color" If you ever coded a UMG button with a text on top of it you will notice that the
-							   // process is quite the same Meaning, you first declare a button which has various events and
-							   // properties and then you place a Text Block widget inside the button's widget to display text
+				.Content()[	   // We create a new slate object inside our button.
 					SNew(STextBlock).Text(FText::FromString("Auto map!"))]];
 }
 
