a14f90325d9ea418f1d1e6f889daee49c93e27e0
AzureAD\JimSimons <jim.simons@ultraleap.com>
2021-10-07 09:40:12 +0100

    Lighting and optimisation on

--- Source/UltraleapTrackingCore/UltraleapTracking.Build.cs
+++ Source/UltraleapTrackingCore/UltraleapTracking.Build.cs
@@ -79,7 +79,7 @@ namespace UnrealBuildTool.Rules
 		public UltraleapTracking(ReadOnlyTargetRules Target) : base(Target)
 		{
 			PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-			OptimizeCode = CodeOptimization.Never;
+			//OptimizeCode = CodeOptimization.Never;
 			PublicIncludePaths.AddRange(
 				new string[] {
 					// ... add public include paths required here ...
