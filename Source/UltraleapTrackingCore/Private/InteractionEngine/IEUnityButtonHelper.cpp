eaed50f7638f58c627c8c4cbae3b576a1b027838
Jim Simons <jim.simons@ultraleap.com>
2022-03-29 08:14:47 +0100

    State into C++

--- Source/UltraleapTrackingCore/Private/InteractionEngine/IEUnityButtonHelper.cpp
+++ Source/UltraleapTrackingCore/Private/InteractionEngine/IEUnityButtonHelper.cpp
@@ -16,13 +16,17 @@ UIEUnityButtonHelper::UIEUnityButtonHelper()
 const float FRICTION_COEFFICIENT = 30;
 const float DRAG_COEFFICIENT = 60;
 
-void UIEUnityButtonHelper::Update(UPARAM(Ref) bool& _pressedThisFrame, UPARAM(Ref) bool& _unpressedThisFrame,
+FVector UIEUnityButtonHelper::constrainDepressedLocalPosition(const FVector& initialLocalPosition, const FVector& localPosition)
+{
+	// Buttons are only allowed to move along their Z axis.
+	return FVector(initialLocalPosition.X, initialLocalPosition.Y, _localPhysicsPosition.Z + localPosition.Z);
+}
+
+void UIEUnityButtonHelper::Update(
 	UPARAM(Ref) bool& ignoreGrasping, UPARAM(Ref) bool& _initialIgnoreGrasping, const bool& isPrimaryHovered, const bool& isGrasped,
 	const bool& controlEnabled, UPARAM(Ref) bool& ignoreContact, UPrimitiveComponent* rigidbody,
-	UPARAM(Ref) FRotator& _initialLocalRotation, const float primaryHoverDistance, UPARAM(Ref) bool& isPressed,
-	UPARAM(Ref) FVector& localPhysicsPosition, UPARAM(Ref) USceneComponent*& _lastDepressor,
-	UPARAM(Ref) FVector& _localDepressorPosition, const float _springForce, const FVector2D& minMaxHeight, const float restingHeight, const float WorldDelta, const FVector& initialLocalPosition, UPARAM(Ref) FVector& physicsPosition,
-	UPARAM(Ref) FVector& _physicsVelocity, UPARAM(Ref) float& _pressedAmount)
+	const FRotator& _initialLocalRotation, const float primaryHoverDistance, 
+	const float _springForce, const FVector2D& minMaxHeight, const float restingHeight, const float WorldDelta, const FVector& initialLocalPosition, UPARAM(Ref) float& _pressedAmount, USceneComponent* primaryHoveringController)
 {
 	// Reset our convenience state variables.
 	_pressedThisFrame = false;
@@ -34,35 +38,34 @@ void UIEUnityButtonHelper::Update(UPARAM(Ref) bool& _pressedThisFrame, UPARAM(Re
 
 	// Enforce local rotation (if button is child of non-kinematic rigidbody,
 	// this is necessary).
-	//transform.localRotation = _initialLocalRotation;
 	rigidbody->SetRelativeRotation(_initialLocalRotation);
 	
 	// Record and enforce the sliding state from the previous frame.
-	if (primaryHoverDistance < 0.005f || isGrasped || isPressed)
+	if (primaryHoverDistance < 0.005f || isGrasped || _isPressed)
 	{
-		localPhysicsPosition = constrainDepressedLocalPosition(
-			rigidbody->GetAttachParent()->GetComponentTransform().InverseTransformPosition(rigidbody->GetComponentLocation()) - localPhysicsPosition);
+		_localPhysicsPosition = constrainDepressedLocalPosition(initialLocalPosition,
+			rigidbody->GetAttachParent()->GetComponentTransform().InverseTransformPosition(rigidbody->GetComponentLocation()) - _localPhysicsPosition);
 	}
 	else
 	{
-		FVector2D localSlidePosition = FVector2D(localPhysicsPosition.X, localPhysicsPosition.Y);
+		FVector2D localSlidePosition = FVector2D(_localPhysicsPosition.X, _localPhysicsPosition.Y);
 
-		localPhysicsPosition =
+		_localPhysicsPosition =
 			rigidbody->GetAttachParent()->GetComponentTransform().InverseTransformPosition(rigidbody->GetComponentLocation());
 
-		localPhysicsPosition = FVector(localSlidePosition.X, localSlidePosition.Y, localPhysicsPosition.Z);
+		_localPhysicsPosition = FVector(localSlidePosition.X, localSlidePosition.Y, _localPhysicsPosition.Z);
 	}
 	bool HasVelocity = false;
 	// Calculate the physical kinematics of the button in local space
 	FVector localPhysicsVelocity = rigidbody->GetAttachParent()->GetComponentTransform().InverseTransformVector(rigidbody->GetPhysicsLinearVelocity());
-	if (isPressed && isPrimaryHovered && _lastDepressor != nullptr)
+	if (_isPressed && isPrimaryHovered && _lastDepressor != nullptr)
 	{
 		FVector curLocalDepressorPos =
 			rigidbody->GetAttachParent()->GetComponentTransform().InverseTransformPosition(_lastDepressor->GetComponentLocation());
 		FVector origLocalDepressorPos = rigidbody->GetAttachParent()->GetComponentTransform().InverseTransformPosition(
 			rigidbody->GetComponentTransform().TransformPosition(_localDepressorPosition));
 		localPhysicsVelocity = FVector::BackwardVector * 0.05f;
-		localPhysicsPosition = constrainDepressedLocalPosition(curLocalDepressorPos - origLocalDepressorPos);
+		_localPhysicsPosition = constrainDepressedLocalPosition(initialLocalPosition, curLocalDepressorPos - origLocalDepressorPos);
 	}
 	else if (isGrasped)
 	{
@@ -76,7 +79,7 @@ void UIEUnityButtonHelper::Update(UPARAM(Ref) bool& _pressedThisFrame, UPARAM(Re
 		localPhysicsVelocity +=
 			FMath::Clamp(_springForce * 10000.0f *
 					(initialLocalPosition.Z - FMath::Lerp(minMaxHeight.X, minMaxHeight.Y, restingHeight) -
-								localPhysicsPosition.Z),
+								_localPhysicsPosition.Z),
 				-100.0f / rigidbody->GetAttachParent()->GetComponentScale().X,
 				100.0f / rigidbody->GetAttachParent()->GetComponentScale().X) *
 			WorldDelta * FVector::ForwardVector;
@@ -113,82 +116,84 @@ void UIEUnityButtonHelper::Update(UPARAM(Ref) bool& _pressedThisFrame, UPARAM(Re
 	}
 
 	// Transform the local physics back into world space
-	physicsPosition = rigidbody->GetAttachParent()->GetComponentTransform().TransformPosition(localPhysicsPosition);
+	_physicsPosition = rigidbody->GetAttachParent()->GetComponentTransform().TransformPosition(_localPhysicsPosition);
 	_physicsVelocity = rigidbody->GetAttachParent()->GetComponentTransform().TransformVector(localPhysicsVelocity);
 
 	// Calculate the Depression State of the Button from its Physical Position
 	// Set its Graphical Position to be Constrained Physically
-	bool oldDepressed = isPressed;
+	bool oldDepressed = _isPressed;
 
 	// Normalized depression amount.
-	// TODO FMath::GetMappedRangeValueClamped
-	_pressedAmount = localPhysicsPosition.Z.Map(initialLocalPosition.Z - minMaxHeight.X,
-		initialLocalPosition.X - FMath::Lerp(minMaxHeight.X, minMaxHeight.Y, restingHeight), 1, 0);
+	_pressedAmount = FMath::GetMappedRangeValueClamped(
+		FVector2D(initialLocalPosition.Z - minMaxHeight.X,
+										  initialLocalPosition.X - FMath::Lerp(minMaxHeight.X, minMaxHeight.Y, restingHeight)),
+		FVector2D(1, 0), _localPhysicsPosition.Z);
+
 	if (HasVelocity)
 	{
 		//Debug.Log("localPhysicsPosition " + localPhysicsPosition.ToString("F3") + " " + _pressedAmount.ToString("F3"));
 	}
 	// If the button is depressed past its limit...
-	if (localPhysicsPosition.Z > initialLocalPosition.Z - minMaxHeight.X)
+	if (_localPhysicsPosition.Z > initialLocalPosition.Z - minMaxHeight.X)
 	{
 		rigidbody->SetRelativeLocation(
-			FVector(localPhysicsPosition.X, localPhysicsPosition.Y, initialLocalPosition.Z - minMaxHeight.X));
+			FVector(_localPhysicsPosition.X, _localPhysicsPosition.Y, initialLocalPosition.Z - minMaxHeight.X));
 		if ((isPrimaryHovered && _lastDepressor != nullptr) || isGrasped)
 		{
 			_isPressed = true;
 		}
 		else
 		{
-			physicsPosition = transform.parent.TransformPoint(
-				new Vector3(localPhysicsPosition.x, localPhysicsPosition.y, initialLocalPosition.z - minMaxHeight.x));
+			_physicsPosition = rigidbody->GetAttachParent()->GetComponentTransform().TransformPosition(
+				FVector(_localPhysicsPosition.X, _localPhysicsPosition.Y, initialLocalPosition.Z - minMaxHeight.X));
 			_physicsVelocity = _physicsVelocity * 0.1f;
 			_isPressed = false;
-			_lastDepressor = null;
+			_lastDepressor = nullptr;
 		}
 		// Else if the button is extended past its limit...
 	}
-	else if (localPhysicsPosition.z < initialLocalPosition.z - minMaxHeight.y)
+	else if (_localPhysicsPosition.Z < initialLocalPosition.Z - minMaxHeight.Y)
 	{
-		transform.localPosition =
-			new Vector3(localPhysicsPosition.x, localPhysicsPosition.y, initialLocalPosition.z - minMaxHeight.y);
-		physicsPosition = transform.position;
+		rigidbody->SetRelativeLocation(
+			FVector(_localPhysicsPosition.X, _localPhysicsPosition.Y, initialLocalPosition.Z - minMaxHeight.Y));
+		_physicsPosition = rigidbody->GetComponentLocation();
 		_isPressed = false;
-		_lastDepressor = null;
+		_lastDepressor = nullptr;
 	}
 	else
 	{
 		// Else, just make the physical and graphical motion of the button match
-		transform.localPosition = localPhysicsPosition;
+		rigidbody->SetRelativeLocation(_localPhysicsPosition);
 
 		// Allow some hysteresis before setting isDepressed to false.
-		if (!isPressed || !(localPhysicsPosition.z > initialLocalPosition.z - (minMaxHeight.y - minMaxHeight.x) * 0.1F))
+		if (!_isPressed || !(_localPhysicsPosition.Z > initialLocalPosition.Z - (minMaxHeight.Y - minMaxHeight.X) * 0.1F))
 		{
 			_isPressed = false;
-			_lastDepressor = null;
+			_lastDepressor = nullptr;
 		}
 	}
 
 	// If our depression state has changed since last time...
-	if (isPressed && !oldDepressed)
+	if (_isPressed && !oldDepressed)
 	{
-		primaryHoveringController.primaryHoverLocked = true;
+		//primaryHoveringController.primaryHoverLocked = true;
 		_lockedInteractingController = primaryHoveringController;
 
 		OnPress();
 		_pressedThisFrame = true;
 	}
-	else if (!isPressed && oldDepressed)
+	else if (!_isPressed && oldDepressed)
 	{
 		_unpressedThisFrame = true;
 		OnUnpress();
 
-		if (!(isGrasped && graspingController == _lockedInteractingController))
+	/* if (!(isGrasped && graspingController == _lockedInteractingController))
 		{
 			_lockedInteractingController.primaryHoverLocked = false;
 		}
-
-		_lastDepressor = null;
+			*/
+		_lastDepressor = nullptr;
 	}
 
-	localPhysicsPositionConstrained = transform.parent.InverseTransformPoint(physicsPosition);
+	_localPhysicsPositionConstrained = rigidbody->GetAttachParent()->GetComponentTransform().InverseTransformPosition(_physicsPosition);
 }
--- Source/UltraleapTrackingCore/Private/InteractionEngine/IEUnityButtonHelper.h
+++ Source/UltraleapTrackingCore/Private/InteractionEngine/IEUnityButtonHelper.h
@@ -16,15 +16,55 @@ public:
 	UIEUnityButtonHelper();
 
 protected:
+	UPROPERTY()
+	bool _pressedThisFrame;
 	
-public:	
+	UPROPERTY()
+	bool _unpressedThisFrame;
+		
+	UPROPERTY()
+	bool _isPressed;
+
+	UPROPERTY()
+	FVector _localPhysicsPosition;
+
+	UPROPERTY()
+	USceneComponent* _lastDepressor;
+
+	UPROPERTY()
+	FVector _localDepressorPosition;
+
+	UPROPERTY()
+	FVector _physicsPosition;
+
+	UPROPERTY()
+	FVector _physicsVelocity;
+
 	/**  Logic for IE button from Unity, call on tick from BP */
 	UFUNCTION(BlueprintCallable, Category = "Ultraleap IE")
-	void Update(UPARAM(Ref) bool& _pressedThisFrame);
+	void Update(
+	UPARAM(Ref) bool& ignoreGrasping, UPARAM(Ref) bool& _initialIgnoreGrasping, const bool& isPrimaryHovered, const bool& isGrasped,
+	const bool& controlEnabled, UPARAM(Ref) bool& ignoreContact, UPrimitiveComponent* rigidbody,
+	const FRotator& _initialLocalRotation, const float primaryHoverDistance, 
+	const float _springForce, const FVector2D& minMaxHeight, const float restingHeight, const float WorldDelta, const FVector& initialLocalPosition, UPARAM(Ref) float& _pressedAmount, USceneComponent* primaryHoveringController);
 
 	//UPROPERTY(BlueprintAssignable, EditAnywhere, Category = "Ultraleap IE")
 	//FGrabClassifierGrabStateChanged OnIsGrabbingChanged;
 
+	UPROPERTY()
+	USceneComponent* _lockedInteractingController;
 	
-		
+	UPROPERTY()
+	FVector _localPhysicsPositionConstrained;
+
+
+
+private:
+	void OnPress(){}
+	void OnUnpress(){}
+
+	FVector constrainDepressedLocalPosition(
+		const FVector& initialLocalPosition, const FVector& localPosition);
+
+
 };
