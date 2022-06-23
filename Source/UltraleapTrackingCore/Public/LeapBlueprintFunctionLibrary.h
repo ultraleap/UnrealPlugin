b13d2050a581f85a6c86873404e03d12e42c45b5
Jim Simons <jim.simons@ultraleap.com>
2022-06-23 10:22:25 +0100

    Allows backwards compatible compile to succed for global APIs

--- Source/UltraleapTrackingCore/Public/LeapBlueprintFunctionLibrary.h
+++ Source/UltraleapTrackingCore/Public/LeapBlueprintFunctionLibrary.h
@@ -22,23 +22,23 @@ class ULeapBlueprintFunctionLibrary : public UBlueprintFunctionLibrary
 	GENERATED_UCLASS_BODY()
 
 	/** Set basic global leap tracking options */
-	UFUNCTION(BlueprintCallable, Category = "Ultraleap Tracking Functions")
+	UFUNCTION(BlueprintCallable, Category = "Ultraleap Tracking Functions", meta = (AutoCreateRefTerm = "DeviceSerials"))
 	static void SetLeapMode(ELeapMode Mode, const TArray<FString>& DeviceSerials, ELeapTrackingFidelity Fidelity = ELeapTrackingFidelity::LEAP_NORMAL);
 
 	/** Set global leap options */
-	UFUNCTION(BlueprintCallable, Category = "Ultraleap Tracking Functions")
+	UFUNCTION(BlueprintCallable, Category = "Ultraleap Tracking Functions", meta = (AutoCreateRefTerm = "DeviceSerials"))
 	static void SetLeapOptions(const FLeapOptions& Options, const TArray<FString>& DeviceSerials);
 
 	/** Gets currently set global options */
-	UFUNCTION(BlueprintCallable, Category = "Ultraleap Tracking Functions")
+	UFUNCTION(BlueprintCallable, Category = "Ultraleap Tracking Functions", meta = (AutoCreateRefTerm = "DeviceSerial"))
 	static void GetLeapOptions(FLeapOptions& OutOptions, const FString& DeviceSerial);
 
 	/** Gets Leap read only stats such as api version, frame lookup and device information */
-	UFUNCTION(BlueprintCallable, Category = "Ultraleap Tracking Functions")
+	UFUNCTION(BlueprintCallable, Category = "Ultraleap Tracking Functions", meta = (AutoCreateRefTerm = "DeviceSerial"))
 	static void GetLeapStats(FLeapStats& OutStats, const FString& DeviceSerial);
 
 	/** Change leap policy */
-	UFUNCTION(BlueprintCallable, Category = "Ultraleap Tracking Functions")
+	UFUNCTION(BlueprintCallable, Category = "Ultraleap Tracking Functions", meta = (AutoCreateRefTerm = "DeviceSerials"))
 	static void SetLeapPolicy(ELeapPolicyFlag Flag, bool Enable, const TArray<FString>& DeviceSerials);
 
 	/** List the attached (plugged in) devices */
