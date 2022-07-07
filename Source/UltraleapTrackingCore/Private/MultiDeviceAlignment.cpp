6fa4654c5eccc92063658878c908aba4ba653e4d
Jim Simons <jim.simons@ultraleap.com>
2022-07-07 15:04:08 +0100

    Skeleton classes for device alignment

---
+++ Source/UltraleapTrackingCore/Private/MultiDeviceAlignment.cpp
@@ -0,0 +1,39 @@
+/******************************************************************************
+ * Copyright (C) Ultraleap, Inc. 2011-2021.                                   *
+ *                                                                            *
+ * Use subject to the terms of the Apache License 2.0 available at            *
+ * http://www.apache.org/licenses/LICENSE-2.0, or another agreement           *
+ * between Ultraleap and you, your company or other organization.             *
+ ******************************************************************************/
+
+#include "MultiDeviceAlignment.h"
+
+// Sets default values for this component's properties
+UMultiDeviceAlignment::UMultiDeviceAlignment()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UMultiDeviceAlignment::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+// Called every frame
+void UMultiDeviceAlignment::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
