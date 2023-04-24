0b809ba9e613f2f25ef0c590d15b8e4e099b1f42
YasserNezzari <yasser.nezzari@ultraleap.com>
2023-04-24 13:20:39 +0100

    changing the mesh rotation

--- Source/UltraleapTrackingCore/Private/LeapComponent.cpp
+++ Source/UltraleapTrackingCore/Private/LeapComponent.cpp
@@ -379,10 +379,17 @@ void ULeapComponent::OnDeviceAdded(IHandTrackingWrapper* DeviceWrapper)
 	{
 		SubscribeToDevice();
 	}
+
+	if (DeviceWrapper->GetDeviceSerial() == ActiveDeviceSerial)
+	{
+		ConnectToInputEvents();
+	}
+	else
+	{
+		RefreshDeviceList(true);
+	}
 	ActiveDeviceSerial = DeviceWrapper->GetDeviceSerial();
 
-	RefreshDeviceList(true);
-	
 }
 void ULeapComponent::OnDeviceRemoved(IHandTrackingWrapper* DeviceWrapper)
 {
